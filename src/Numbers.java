bf70a480243f2a1ae3d3ab5bb466ad8b1a10fb23
Jose Manuel Alique <josealique@gmail.com>
2017-11-07 19:40:37 +0100

    Numbers de java

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,38 +1,161 @@
+import java.util.Arrays;
 
 public class Numbers {
 
+    static String[] hasta19 = {"One", "Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    static String[] minusculas = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    static String[] de20hasta90 = {"", "Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    static String[] minusculas2 = {"", "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
 
-    public static String say(long n) {
-        switch((int) n) {
-        case 0: return "Zero";
-        case 1: return "One";
-        case 2: return "Two";
-        case 3: return "Three";
-        case 4: return "Four";
-        case 5: return "Five";
-        case 6: return "Six";
-        case 7: return "Seven";
-        case 8: return "Eighteen";
-        case 9: return "Nine";
-        case 10: return "Ten";
-        case 11: return "Eleven";
-        case 12: return "Twelve";
-        case 13: return "Thirthen";
-        case 14: return "Fourteen";
-        case 15: return "Fifteen";
-        case 16: return "Sixteen";
-        case 17: return "Seventeen";
-        case 18: return "Eighteen";
-        case 19: return "Nineteen";
-        case 20: return "Twenty";
-
+    public static void main(String[] args) {
+        System.out.println(say(986_678));
     }
-    return"No pot ser";
-}
+    public static String say(long n) {
+        if (n <= 999){
+            return hastaMil((int) n);
+        } else if (n <= 999_999){
+            return hastaMillon((int)n);
+        } else if (n <= 999_999_999){
+            return hastaBillon((int)n);
+        }
+        return "";
+    }
+
+   static String hastaMil(int n) {
+       String vacia = "";
+       if (n == 0){
+           return "Zero";
+       }
+        if (n < 20){
+           vacia += hasta19[n-1];
+       } else if ( n >= 20 && n <= 99){
+           if ( n % 10 == 0){
+               vacia += de20hasta90[n/10-1];
+           } else {
+               vacia += de20hasta90[n/10-1]+"-"+minusculas[n % 10-1];
+           }
+       } else if (n <= 999){
+            if (n % 100 == 0){
+                vacia += hasta19[n/100-1] + " hundred";
+            } else if (n % 100 <= 19) {
+                vacia += hasta19[n / 100 - 1] + " hundred and " + minusculas[n % 100 - 1];
+            } else {
+                if (n % 10 == 0){
+                    vacia += hasta19[n/100-1] + " hundred and "+ minusculas2[n % 100-1];
+                } else {
+                    vacia += hasta19[n / 100-1]+" hundred and "+minusculas2[n % 100 / 10-1]+"-"+minusculas[n % 10-1];
+                }
+            }
+        }
+       return vacia;
+   }
+
+   static String hastaMilMinus(int n) {
+        String vacia = "";
+        if (n < 20){
+            vacia += minusculas[n-1];
+        } else if ( n >= 20 && n <= 99){
+            if ( n % 10 == 0){
+                vacia += minusculas2[n/10-1];
+            } else {
+                vacia += minusculas2[n/10-1]+"-"+minusculas[n % 10-1];
+            }
+        } else if (n <= 999){
+            if (n % 100 == 0){
+                vacia += minusculas[n/100-1] + " hundred";
+            } else if (n % 100 <= 19) {
+                vacia += minusculas[n / 100 - 1] + " hundred and " + minusculas[n % 100 - 1];
+            } else {
+                if (n % 10 == 0){
+                    vacia += minusculas[n/100-1] + " hundred and "+ minusculas2[n % 100-1];
+                } else {
+                    vacia += minusculas[n / 100-1]+" hundred and "+minusculas2[n % 100 / 10-1]+"-"+minusculas[n % 10-1];
+                }
+            }
+        }
+        return vacia;
+    }
+
+   static String hastaMillon(int n){
+       String vacia = "";
+       if (n / 1000 <= 19){
+           vacia += hasta19[n/1000-1] + " thousand";
+       } else if (n / 1000 <= 99){
+           if ( n / 1000 % 10 == 0){
+               vacia += de20hasta90[n/1000 / 10-1 ]+ " thousand";
+           } else {
+               vacia += de20hasta90[n/1000 / 10-1]+"-"+minusculas[n/1000 % 10-1]+" thousand";
+           }
+       } else {
+           if ( n / 1000 % 100 == 0){
+               vacia += hasta19[n/1000 / 100 -1]+ " hundred thousand";
+           } else if (n / 1000 % 100 <= 19){
+               vacia += hasta19[n/1000 / 100 -1] + " hundred and "+minusculas[n/1000 % 100-1]+ " thousand";
+           } else {
+               if (n / 1000 % 10 == 0){
+                   vacia += hasta19[n/1000 / 100-1] + " hundred "+minusculas2[n / 1000 % 100 / 10-1]+ " thousand";
+               } else {
+                   vacia += hasta19[n/1000 / 100-1] + " hundred "+ minusculas2[n / 1000 % 100 / 10-1]+ "-" + minusculas[n / 1000 % 10]+" thousand";
+               }
+           }
+       }
+       if (n % 1000 == 0){
+           return vacia;
+       } else {
+           return vacia+" "+hastaMilMinus(n%1000);
+       }
+    }
+
+    static String hastaMillonMinus(int n){
+        String vacia = "";
+        if (n / 1000 <= 19){
+            vacia += minusculas[n/1000-1] + " thousand";
+        } else if (n / 1000 <= 99){
+            if ( n / 1000 % 10 == 0){
+                vacia += minusculas2[n/1000 / 10-1 ]+ " thousand";
+            } else {
+                vacia += minusculas2[n/1000 / 10-1]+"-"+minusculas[n/1000 % 10-1]+" thousand";
+            }
+        } else {
+            if ( n / 1000 % 100 == 0){
+                vacia += minusculas[n/1000 / 100 -1]+ " hundred thousand";
+            } else if (n / 1000 % 100 <= 19){
+                vacia += minusculas[n/1000 / 100 -1] + " hundred and "+minusculas[n/1000 % 100-1]+ " thousand";
+            } else {
+                if (n / 1000 % 10 == 0){
+                    vacia += minusculas[n/1000 / 100-1] + " hundred "+minusculas2[n / 1000 % 100 / 10-1]+ " thousand";
+                } else {
+                    vacia += minusculas[n/1000 / 100-1] + " hundred "+ minusculas2[n / 1000 % 100 / 10-1]+ "-" + minusculas[n / 1000 % 10]+" thousand";
+                }
+            }
+        }
+        if (n % 1000 == 0){
+            return vacia;
+        } else {
+            return vacia+" "+hastaMilMinus(n%1000);
+        }
+    }
+
+    static String hastaBillon(int n){
+        String vacia = "";
+        int millon = n / 1_000_000;
+        if (millon <= 19){
+            vacia += hasta19[millon-1] + " million";
+        } else if (millon <= 99){
+            if (millon % 10 == 0){
+                vacia += de20hasta90[millon / 10-1] + " million";
+            } else {
+                vacia += de20hasta90[millon / 10-1]+"-"+minusculas[millon % 10-1]+" million";
+            }
+        } else {
+            if (millon % 100 == 0){
+                vacia += hasta19[millon / 100-1]+" hundred million";
+            } else if( millon / 100)
+        }
+    }
+
 
     public static long words(String s) {
         return 0;
     }
-
-
 }
